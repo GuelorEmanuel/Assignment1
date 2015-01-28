94edefa1820b33f8f685eaa41b6873dc92a319ae
Comp1601 <Comp1601@Comp1601s-Mac-Pro.local>
2015-01-28 15:06:23 -0800

    Initial Commit

---
+++ Assignment1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment1
+//
+//  Created by Comp1601 on 2015-01-28.
+//  Copyright (c) 2015 Comp1601. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
